728cf2b9fa3118cb6bf4121958e175a6ba145795
yuna <nana0744@hyconsy.com>
2021-10-19 18:46:40 +0900

    react

---
+++ tutorial/begin-react/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Hello from './Hello'; //상대경로
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       React Tutorial
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ tutorial/begin-react/src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'; //리액트 불러와서 사용하겠다고 선언
+
+//컴포넌트 만들기
+function Hello() {
+    //xml 형태의 값을 리턴 
+    return <div>안녕하세요</div>; //jsx
+}
+
+export default Hello; //hello라는 함수를 만들어서 내보내겠다 선언
