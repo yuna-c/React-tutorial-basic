b6632592ea6f2e958b8076638668853138d35540
yuna <nana0744@hyconsy.com>
2021-11-01 19:00:03 +0900

    react

--- tutorial/begin-react/src/App.js
+++ tutorial/begin-react/src/App.js
@@ -1,6 +1,15 @@
-import React, { useRef, useMemo, useCallback, useReducer } from 'react';
+import React, { useRef,  useReducer, useMemo, useCallback } from 'react';
 import UserList5 from './UserList5';
 import CreateUser from './CreateUser';
+
+
+
+//사용자 카운트
+function countActiveUsers(users) {
+    console.log('활성 사용자 수를 세는중...');
+    return users.filter(user => user.active).length;
+  }
+  
 
 // useReducer 구현하기 
 const initialState = {
@@ -32,7 +41,7 @@ const initialState = {
 
 // reducer 함수의 틀 만들기
 function reducer(state, action) {
-    switch(action.type) {
+    switch (action.type) {
         case 'CHANGE_INPUT' :
             return {
                 ...state,
@@ -45,6 +54,20 @@ function reducer(state, action) {
             return {
                 inputs : initialState.inputs,
                 users : state.users.concat(action.user)
+            }
+        case 'TOGGLE_USER' :
+            return {
+                ...state,
+                users : state.users.map( user => 
+                    user.id === action.id 
+                    ? {...user, active: !user.active}
+                    : user
+                )
+            };
+        case 'REMOVE_USER' : 
+            return {
+                ...state,
+                users : state.users.filter(user => user.id !== action.id)
             }
         default : 
             // return state;
@@ -80,6 +103,21 @@ function App() {
         nextId.current += 1;
     }, [username, email]);
 
+    const onToggle = useCallback(id => {
+        dispatch({
+            type : 'TOGGLE_USER',
+            id
+        });
+    }, []);
+
+    const onRemove = useCallback(id => {
+        dispatch({
+            type : 'REMOVE_USER',
+            id
+        }, []);
+    });
+    
+    const count = useMemo(() => countActiveUsers(users), [users]);
     return (
         <>
             <CreateUser 
@@ -88,8 +126,12 @@ function App() {
                 onChange={onChange}
                 onCreate={onCreate}
             />
-            <UserList5 users={users} />
-            <div>활성 사용자 수: 0</div> 
+            <UserList5 
+                users={users} 
+                onToggle={onToggle} 
+                onRemove={onRemove}
+            />
+            <div>활성 사용자 수: {count}</div> 
         </>
     
     );
